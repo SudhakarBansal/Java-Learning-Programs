e0fdedf1cb573a1670537029d150bbec74ba1435
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2023-08-13 10:17:21 +0530

    Practice programs

---
+++ BasicJava.java
@@ -0,0 +1,8 @@
+package practice_programs;
+//THis is a basic Java Program 
+public class BasicJava {
+    public static void main(String[] args){
+        System.out.println("This program is made by Sudhakar Bansal\nRoll no : 2231");
+        System.out.println("Hello World");
+    }
+}
