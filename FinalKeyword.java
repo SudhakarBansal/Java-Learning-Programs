db5d60e3c4bf92ba50bf4dc5ae4298af003900db
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2023-08-29 16:09:30 +0530

    Final variables in Java

---
+++ FinalKeyword.java
@@ -0,0 +1,9 @@
+package practice_programs;
+class FinalKeyword {
+    public static void main(String args[]) {
+        final int age = 45;
+        // age++;   --->this will give an error
+        System.out.println("The age is : " + age);
+
+    }
+}
